e81b1010e4f4015ab82431dc2a66bea59bf6d6ca
olakunle.olatunji <olatunji.olakunle@cyphercrescent.com>
2025-10-13 10:03:17 +0100

    fix: home session query

--- src/services/sessions.service.ts
+++ src/services/sessions.service.ts
@@ -12,7 +12,9 @@ export const fetchSessions = async ({
 
 export const fetchSessionsForHome = async () => {
 	const response = await api.get<Promise<PaginatedResponse<ISession[]>>>(
-		'/sessions?filters[$or][0][state][$eq]=live&filters[$or][1][state][$eq]=live&pagination[pageSize]=2&pagination[page]=1&sort[0]=state:asc'
+		'/sessions?pagination[pageSize]=2&pagination[page]=1&sort[0]=state:asc&status=published'
 	);
 	return (await response.data).data;
 };
+
+// filters[$or][0][state][$eq]=live&filters[$or][1][state][$eq]=live&
